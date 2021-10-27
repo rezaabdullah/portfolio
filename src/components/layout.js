2b1f733b1ef2c0270fdadf9b0cb49856cb6d36a6
rezaabdullah <air.reza@hotmail.com>
2021-10-27 17:03:13 +0600

    feat: Added typewriting effect

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,13 @@ import * as React from "react"
 import { Container,
     Hero,
     Section_title,
-    Image_wrapper
+    Image_wrapper,
+    // typing_container,
+    // input_cursor
 } from "./ui/layout.module.scss"
 import Navbar from "./navbar"
+// import { StaticImage } from 'gatsby-plugin-image'
+import Typewriter from "typewriter-effect";
 
 const Layout = ({ pageTitle, children }) => {
     return (
@@ -13,12 +17,18 @@ const Layout = ({ pageTitle, children }) => {
             <main>
                 <section id={Hero}>
                     <div className={Section_title}>
-                        <h1>{pageTitle}</h1>
-                        {/* <h2>This is a random text</h2> */}
+                        <h1>Hi, I'm Abdullah Reza</h1>
+                        <h2>
+                            <Typewriter
+                                options={{
+                                    strings: ["Product Manager", "Data Scientist", "Mech. Engineer", "Philomath"],
+                                    autoStart: true,
+                                    loop: true
+                                }}
+                            />
+                        </h2>
                     </div>
-                    <div className={Image_wrapper}>
-                        <h1>{pageTitle}</h1>
-                    </div>
+                    <div className={Image_wrapper}></div>
                 </section>
             </main>
         </div>
