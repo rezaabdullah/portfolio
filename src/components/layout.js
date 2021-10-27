fca5618e1dc15576d93a293ff0f0b29dcb407e3a
rezaabdullah <air.reza@hotmail.com>
2021-10-27 11:14:03 +0600

    refactor: Modularized navigation bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,44 +1,35 @@
 import * as React from "react"
+// import { Container,
+//     Nav_wrapper,
+//     logo_wrapper,
+//     Nav_wrapper__ul,
+//     Hero
+// } from "./ui/layout.module.scss"
 import { Container,
-    Nav_wrapper,
-    logo_wrapper,
-    Nav_wrapper__ul
+    Hero
 } from "./ui/layout.module.scss"
+import Navbar from "./navbar"
 
 const Layout = ({ pageTitle, children }) => {
     return (
         <div className={Container}>
-            <nav className={Nav_wrapper}>
+            {/* <nav className={Nav_wrapper}>
                 <div className={logo_wrapper}>
                     A|R
                 </div>
                 <ul className={Nav_wrapper__ul}>
-                    <li>Home</li>
-                    <li>Projects</li>
-                    <li>Blogs</li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Projects</a></li>
+                    <li><a href="/">Blogs</a></li>
                 </ul>
-            </nav>
-            <main></main>
+            </nav> */}
+            <Navbar/>
+            <main>
+                <section id={Hero}>
+                    <h1>{pageTitle}</h1>
+                </section>
+            </main>
             <footer></footer>
-            {/* <nav class={nav-wrappper}>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-            <div>
-                <title>{pageTitle}</title>
-                    <main>
-                        <h1>{pageTitle}</h1>
-                        {children}
-                    </main>
-            </div>
-            <footer>
-                <p>This is a footer</p>
-            </footer> */}
         </div>
     )
 }
