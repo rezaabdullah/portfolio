4827228a662a731c23f29d288bd33bec0212cecf
rezaabdullah <air.reza@hotmail.com>
2021-10-28 02:54:41 +0600

    style: Added social media icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,10 @@ import { Container,
     Hero,
     Section_title,
     Image_wrapper,
-    Blog
+    social_media
 } from "./ui/layout.module.scss"
 import Navbar from "./navbar"
-// import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
 import Typewriter from "typewriter-effect";
 
 const Layout = ({ pageTitle, children }) => {
@@ -27,11 +27,15 @@ const Layout = ({ pageTitle, children }) => {
                             />
                         </h2>
                         {children}
+                        <div className={social_media}>
+                            <ul>
+                                <li><a href="https://www.linkedin.com/in/airreza/"><StaticImage src="../images/linkedin.png" alt="LinkedIn" height="32"/></a></li>
+                                <li><a href="https://medium.com/@abdullahreza"><StaticImage src="../images/medium.png" alt="Medium" height="32"/></a></li>
+                                <li><a href="https://github.com/rezaabdullah"><StaticImage src="../images/logo_github.png" alt="Github" height="32"/></a></li>
+                            </ul>
+                        </div>
                     </div>
                     <div className={Image_wrapper}></div>
-                </section>
-                <section class={Blog}>
-
                 </section>
             </main>
         </div>
